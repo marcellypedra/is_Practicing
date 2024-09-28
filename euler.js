b98a538328e6132d2d08d939f8c09c1b69d07b2b
marcellypedra <20040674@mydbs.ie>
2024-09-28 09:57:42 +0100

    create function

--- euler.js
+++ euler.js
@@ -13,3 +13,6 @@ alert("Loaded Euler JS");
 a=3;
 b=5;
 n=1000;
+
+    
+}
---
+++ euler1.js
@@ -0,0 +1,26 @@
+//Find the sum of all the multiples of 3 or 5 below 1000.
+
+//Crete a function to find the sum of all the multiples of a or 
+//(a, b, n) are function parameters
+
+//Allow the user to enter the parameters via text box(es) and alert them to the output
+
+function findamultiples(integer, limit) {
+    let cnt=1
+    multi=integer
+    let result = []
+
+    while (multi < 1000) {
+        result.push(multi)
+        cnt++
+        multi = cnt*integer
+    }
+    console.log(result)
+    return result
+        
+    }
+    
+findamultiples(integer:[3,5], limit:1000)
+
+
+
