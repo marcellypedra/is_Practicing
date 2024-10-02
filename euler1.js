147b4e5afb71909765723f6ab3918cf919de66cb
marcellypedra <20040674@mydbs.ie>
2024-10-02 21:55:17 +0100

    change position alert and deleted return variable

--- euler1.js
+++ euler1.js
@@ -15,10 +15,10 @@ let multiplessum = () => {
     for (i = 0; i < Numbern; i++) {
         if ((i % Numbera === 0) || (i % Numberb === 0)) {
         sumnumbers += i; 
-alert("The sum of all multiples is: " + sumnumbers); 
+ 
         }  
     }     
-   return sumnumbers
+    alert("The sum of all multiples is: " + sumnumbers)
 }
 
 
