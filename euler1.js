888ded201073d9645fa3ac0908aa767a4f74a2c8
marcellypedra <20040674@mydbs.ie>
2024-10-02 22:42:00 +0100

    deleted extra brackets

--- euler1.js
+++ euler1.js
@@ -13,10 +13,9 @@ let multiplessum = () => {
     let Numberb = document.getElementById("Numberb").value;
     let Numbern = document.getElementById("Numbern").value;
     for (i = 0; i < Numbern; i++) {
-        if ((i % Numbera === 0) || (i % Numberb === 0)) {
+        if ((i % Numbera === 0) || (i % Numberb === 0)) 
         sumnumbers += i; 
-        }
-
+       
     alert("The sum of all multiples is: " + sumnumbers)
 
     }     
