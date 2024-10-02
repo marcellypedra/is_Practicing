897326fc76091df3438328d7b14de90a2f76a6f7
marcellypedra <20040674@mydbs.ie>
2024-10-02 21:25:29 +0100

    changed position in function

--- euler1.js
+++ euler1.js
@@ -14,8 +14,8 @@ let multiplessum = () => {
     let Numbern = document.getElementById("Numbern").value;
     for (i = 0; i < Numbern; i++) {
         if ((i % Numbera == 0) || (i % Numberb == 0)) 
-        sumnumbers += i;  
-        return sumnumbers 
+             sumnumbers += i;  
+        
     }
     let result = multiplessum;
     alert("The sum of all multiples is: " + result)
