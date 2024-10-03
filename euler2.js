d9cd81b7b37d0bed6357b4b20771807459859793
marcellypedra <20040674@mydbs.ie>
2024-10-03 18:14:26 +0100

    included commandreturn to push into html

--- euler2.js
+++ euler2.js
@@ -14,6 +14,7 @@ let multiplessum = () => {
     for (i = 0; i < Numbern; i++) {
         if ((i % Numbera === 0) || (i % Numberb === 0)) 
         sumnumbers += i; 
+return sumnumbers
        
     alert("The sum of all multiples is: " + sumnumbers)
 
