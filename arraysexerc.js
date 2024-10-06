c3232b8033690e4d4e970910c3123efd3d516010
marcellypedra <20040674@mydbs.ie>
2024-10-06 10:02:22 +0100

    created function every for problem a

---
+++ arraysexerc.js
@@ -0,0 +1,18 @@
+let sumnumbers = 0;
+
+let calculate =() => {
+let a = 3; 
+let b = 5;
+let matches = true;
+sumnumbers.forEach(i => { 
+    if (!calculate(i) { matches = (i % a == 0) || (i % b == 0); return;} ) 
+} );
+return matches; 
+
+}
+
+let l = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+l.every(i => i++);
+console.log(sumnumbers)
+
+
