bd3dbd99dfea4bfb82761f4277eee3187e75b0c2
marcellypedra <20040674@mydbs.ie>
2024-10-06 10:47:29 +0100

    updated formula

--- arraysexerc.js
+++ arraysexerc.js
@@ -3,11 +3,12 @@ let sumnumbers = 0;
 let calculate =() => {
 let a = Number(document.getElementById("numbera").value); 
 let b = Number(document.getElementById("numberb").value);
-let l = Array (getElementById("startlist")
+let l = Number(getElementById("list").value);
 let matches = true;
 sumnumbers.forEach(i => { 
-    if (!calculate(i) { matches = (i % a == 0) || (i % b == 0); return;} ) 
-} );
+    if (!calculate(i) { matches = (i % a == 0) || (i % b == 0), i < l; 
+        return; })  
+})
 return matches; 
 
 }
