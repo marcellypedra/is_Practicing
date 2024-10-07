4f9235e0ea6d98e497a4593db5899b5002a00bc3
marcellypedra <20040674@mydbs.ie>
2024-10-07 11:46:07 +0100

    included alert

--- index2.js
+++ index2.js
@@ -2,10 +2,13 @@ const a = [Number(document.getElementById("numbera").value)];
 const b = [Number(document.getElementById("numberb").value)];
 const l = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-const summulti = l.reduce ((p, c) => {
+const summulti = l.reduce {((p, c) => {
     const multi = l.filter ((c % a === 0) || (c % b === 0));
 
-     return p + c
-
+     return p + c;
     }, 0)
-alert("The result is: " + summulti)
+
+     alert ("The result is: " + summulti)
+
+}
+
