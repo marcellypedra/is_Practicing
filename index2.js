9141f85924e2b368aa4eaca8d90521cf2f92fa12
marcellypedra <20040674@mydbs.ie>
2024-10-10 14:39:48 +0100

    updated for function

--- index2.js
+++ index2.js
@@ -1,10 +1,10 @@
-let calculate = () => {
-let sumnumbers = 0;
+function calculate() {
+    let sumnumbers = 0;
 
     let a = Number(document.getElementById("numbera").value);
     let b = Number(document.getElementById("numberb").value);
     let l = $(document.getElementsByName("list").value);
-};
+}
 
 
 
