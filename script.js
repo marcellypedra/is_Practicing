01a0ae39520c297b5369925c925cad197b3e0774
marcellypedra <20040674@mydbs.ie>
2024-10-10 16:40:56 +0100

    updated function to get elements from different input and tranform in one array

--- script.js
+++ script.js
@@ -1,33 +1,3 @@
-//Euler Problem
-//Find the sum of all the multiples of 3 or 5 below 1000.
-
-//Crete a function to find the sum of all the multiples of a or 
-//(a, b, n) are function parameters
-
-//Allow the user to enter the parameters via text box(es) and alert them to the output
-
-//Demostrate some DOM manipulation in the exercise. 
-//For example changing the value of a result div , hiding inputs once the button is clicked..let sumnumbers = 0;
-
-//let multiplessum = () => {
-
-    //sumnumbers = 0;
-
-    //let Numbera = Number(document.getElementById("Numbera").value);
-    //let Numberb = Number(document.getElementById("Numberb").value);
-    //let Numbern = Number(document.getElementById("nlista").value);
-    //for (i = 0; i < Numbern; i++) {
-        //if ((i % Numbera == 0) || (i % Numberb == 0)) {
-        //sumnumbers += i; 
-        }
-    
-    } 
-     //alert("The sum of all multiples is: " + sumnumbers);
-
-    
-     //document.getElementById("resultbox").value = sumnumbers;
-}
-
 //Arrays:
 //a.
 //Create a function which takes parameters a,b,l
@@ -46,6 +16,7 @@
 //l is a list of integers
 //find the sum of all the multiples of elements of a in l
 
+let sumnumbers = 0;
 
 let multiplessum = () => {
 
@@ -53,25 +24,25 @@ let multiplessum = () => {
 
     let Numbera = Number(document.getElementById("Numbera").value);
     let Numberb = Number(document.getElementById("Numberb").value);
-    let Numberl = () => {
-        let l = document.getElementsByName("nlist");
-        let inputArray = [];
+    const l = (document.getElementById("nlista").value, document.getElementById("nlistb").value, document.getElementById("nlistc").value)
+        let inputArray = []        
         for (i = 0; i < l.length; i++) {
             let value = l[i].value.trim(); 
                 if (value) { 
                     inputArray.push(value);
                 }
             }
-            document.getElementById("result").innerText = "Values: " + inputArray.join(", ");  
-            //let value
-           // if ((i % Numbera == 0) || (i % Numberb == 0)) {
-                //sumnumbers += i;
-           // }
+            let result = inputArray.join(", ");
 
-        }
-        //alert("The sum of all multiples is: " + sumnumbers);
-
-
-        //document.getElementById("resultbox").value = sumnumbers;
+    for (i = 0; i < result.length; i++) {
+       if ((i % Numbera == 0) || (i % Numberb == 0)) {
+        sumnumbers += i;              
     }
+     
+     }
+         alert("The sum of all multiples is: " + sumnumbers);
+
+         document.getElementById("resultbox").value = sumnumbers;
+}
+      
 
