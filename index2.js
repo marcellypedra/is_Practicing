4f54571aa7b75ad0bb6800d6046baf7437e9eb8c
marcellypedra <20040674@mydbs.ie>
2024-10-10 14:02:47 +0100

    updated formula

--- index2.js
+++ index2.js
@@ -1,8 +1,10 @@
 let sumnumbers = 0;
 
-let calculate =() => {
-let a = Number(document.getElementById("numbera").value); 
-let b = Number(document.getElementById("numberb").value);
-let l = $(document.getElementsByName("list").value);
+function calculate() {
+    let a = Number(document.getElementById("numbera").value);
+    let b = Number(document.getElementById("numberb").value);
+    let l = $(document.getElementsByName("list").value);
+};
+console.log(calculate);
 
 
