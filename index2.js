6fe6d3cd82742556bf95d71c6d6ab793d1a98287
marcellypedra <20040674@mydbs.ie>
2024-10-10 14:29:53 +0100

    updated the function

--- index2.js
+++ index2.js
@@ -1,6 +1,6 @@
+let calculate = () => {
 let sumnumbers = 0;
 
-function calculate() {
     let a = Number(document.getElementById("numbera").value);
     let b = Number(document.getElementById("numberb").value);
     let l = $(document.getElementsByName("list").value);
